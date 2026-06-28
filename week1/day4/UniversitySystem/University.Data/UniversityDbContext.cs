571c3c49dd43a6657d2627ff0c90e34e046abc53
marah-aljumaat <marahaljumaat@gmail.com>
2026-06-28 11:27:48 +0300

    finishing part-1

---
+++ week1/day4/UniversitySystem/University.Data/UniversityDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using University.Data.Configurations;
+using University.Data.Entities;
+
+namespace University.Data
+{
+    public class UniversityDbContext : DbContext
+    {
+        public DbSet<Student> Students { get; set; }
+
+        public UniversityDbContext(DbContextOptions<UniversityDbContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(UniversityDbContext).Assembly);
+        }
+    }
+}
